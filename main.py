e9757c827c2f9a2ae4269c122c17c1d0ef0dd4cb
Ponte-Matias <112644445+Ponte-Matias@users.noreply.github.com>
2025-04-06 17:43:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/cuenta")
+async def root():
+    a=0
+    for i in range(100):
+        a+=i
+    print(a)
+    return {"message": "Se realizó el cálculo"}    
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
